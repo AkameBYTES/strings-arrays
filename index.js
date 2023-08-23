372a37064466548bc035181df379de441ac87b39
アカメ <141885614+AkameBYTES@users.noreply.github.com>
2023-08-22 21:52:33 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,75 @@
+//Exercicíos de Strings e Arrays
+
+//Exercicío 1
+/*a.  undefined
+b.  null
+c.  11
+d.  3
+e.  (11) [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+f.  9
+undefined
+*/
+
+/*Exercicío 2
+const frase = prompt("Digite uma frase")
+console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
+com esse código, vai deixar tudo maiúsculo, vai trocar o A por I e vai mostrar a quantidade de palavras neste código
+*/
+
+//Exercicío 3
+/*const nomeUsuario = prompt('Qual o seu nome?')
+const emailUsuario = prompt('Qual o seu email?')
+console.log('O e-mail ' +  emailUsuario +  ' foi cadastrado com sucesso. Seja bem-vindo ' +  nomeUsuario ) 
+O e-mail akamebytes@hotmail.com foi cadastrado com sucesso. Seja bem-vindo Akame
+Resultado da atividade
+*/
+
+//Exercicío 4
+/*const comidasFavoritas = ["Strogonoff", "Sushi", "Lasanha", "Uva", "Torta-fria"]
+console.log(comidasFavoritas)
+console.log(`Essas são minhas comidas favoritas: 
+${comidasFavoritas[0]}, 
+${comidasFavoritas[1]}, 
+${comidasFavoritas[2]}, 
+${comidasFavoritas[3]}, 
+${comidasFavoritas[4]}`)
+
+const comidaUsuario = prompt("Qualé tua comida favorita pivete?")
+console.log(comidaUsuario)
+let i = 0
+comidasFavoritas [i+1] = comidaUsuario
+console.log(comidasFavoritas)
+*/
+
+//Exercicío 5
+/*const listaDetarefas = []
+const pergunta1 = prompt("Qual a primeira de hoje?")
+const pergunta2 = prompt ("Qual a segunda de hoje?")
+const pergunta3 = prompt("Qual a última de hoje?")
+console.log(pergunta1, pergunta2, pergunta3)
+
+listaDetarefas[0] = pergunta1
+listaDetarefas[1] = pergunta2
+listaDetarefas[2] = pergunta3
+console.log(listaDetarefas)
+
+const tarefaRealizada = prompt("Qual dessas tu fez?")
+let i = 0
+listaDetarefas.splice(i,1)
+console.log(listaDetarefas)
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
